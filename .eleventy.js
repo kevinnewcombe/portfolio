411f3c13cf7a599d57e9f640f6de83d6f4e2c2a3
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-04-19 10:47:33 -0600

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+
+const htmlmin = require("html-minifier");
+const CleanCSS = require("clean-css");
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./favicon.ico"); // add favicon to the build
+
+	// minify css
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
+
+	// minify html
+	eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,  
+        useShortDoctype: true,
+      });
+    }
+    return content;
+  });
+  eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+
+	return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  }
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# kevin-newcombe.com
+'Coming eventually' page built in [eleventy](https://www.11ty.dev). Deploys from Netlify.
+
+## Scripts
+* Install dependencies: `npm install`
+* Run dev server: `npx @11ty/eleventy --serve`
+* Build: `npx @11ty/eleventy`
