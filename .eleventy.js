f8ec49d4a8b6b812c370804fd3fc587aebeec16f
Kevin Newcombe <kevinjnewcombe@gmail.com>
2024-04-19 10:34:31 -0600

    Add github logo

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,31 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "favicon.ico": "/" });
+  eleventyConfig.addPassthroughCopy("./favicon.ico");
+};
+
+
+// minify inlined css
+const CleanCSS = require("clean-css");
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
+};
+
+const htmlmin = require("html-minifier");
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,  
+        useShortDoctype: true,
+      });
+    }
+
+    return content;
+  });
+};
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
 };
